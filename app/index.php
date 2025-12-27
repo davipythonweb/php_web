1364293a36bde09eefdc84d038564545e247a9bd
davipythonweb <galaxys3tab32@gmail.com>
2025-12-27 04:54:01 -0300

    update first projeto php

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando Um projeto</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <?php
+    /**
+     * Bloco PHP com variavel start
+     * e comando de saida echo.
+     */
+    $start = "Vamos começar";
+    echo "<h1> Ola mundo {$start}</h1>";
+    echo "<p id='js' >Loading...</p>";
+    ?>
+   <script src="asssets/scripts.js"></script>
+</body>
+</html>
